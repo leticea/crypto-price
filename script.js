0763c746a87fffebf98af64f17e4ca8d0d681253
leticea <nascimento.leticeah@gmail.com>
2023-02-23 09:35:52 -0300

    styles code

--- script.js
+++ script.js
@@ -7,18 +7,24 @@ fetch(
     const coins = Object.getOwnPropertyNames(json);
 
     for (let coin of coins) {
-      const coinInfo = json [`${coin}`];
+      const coinInfo = json[`${coin}`];
       const price = coinInfo.usd;
       const change = coinInfo.usd_24_change.toFixed(5);
 
       container.innerHTML += `
-        <div class="coin ${change < 0 ? 'falling' : 'rising'}">
+        <div class="coin ${change < 0 ? "falling" : "rising"}">
           <div class="coin-logo">
             <img src="images/${coin}.png">
           </div>
           <div class="coin-name">
             <h3>${coin}</h3>
             <span>/USD</span>
-      `
+          </div>
+          <div class="coin-price">
+            <span class="price">$${price}</span>
+            <span class="change">$${change}</span>
+          </div>
+        </div>
+      `;
     }
   });
